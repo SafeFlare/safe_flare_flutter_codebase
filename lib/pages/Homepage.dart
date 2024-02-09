c5a8ec602a424c8f6f710e42f6c616162c442c21
Sugama1001 <baturajaahmad@gmail.com>
2024-02-10 01:09:48 +0700

    -Add Profile menu in Homepage.dart -Add Notification logo in header -Add const to improve performance -use sdk '>=3.2.3 <4.0.0' version

--- lib/pages/Homepage.dart
+++ lib/pages/Homepage.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
 
 class Homepage extends StatefulWidget {
-
   @override
   State<Homepage> createState() => _Homepage();
 }
 
 class _Homepage extends State<Homepage> {
-
   int _currentIndex = 0;
 
   late BuildContext context;
@@ -15,146 +13,322 @@ class _Homepage extends State<Homepage> {
   static List<Map<String, dynamic>> news = [
     {
       "News": "Berita kebakaran 1",
-      "imageURL": "https://bpba.acehprov.go.id/thumbnail/700x0/media/2024.02/whatsapp_image_2023-12-06_at_1006101.jpeg"
+      "imageURL":
+          "https://bpba.acehprov.go.id/thumbnail/700x0/media/2024.02/whatsapp_image_2023-12-06_at_1006101.jpeg"
     },
     {
       "News": "Berita kebakaran 2",
-      "imageURL": "https://bpba.acehprov.go.id/thumbnail/700x0/media/2024.02/whatsapp_image_2023-12-06_at_1006101.jpeg"
+      "imageURL":
+          "https://bpba.acehprov.go.id/thumbnail/700x0/media/2024.02/whatsapp_image_2023-12-06_at_1006101.jpeg"
     },
     {
       "News": "Berita kebakaran 3",
-      "imageURL": "https://bpba.acehprov.go.id/thumbnail/700x0/media/2024.02/whatsapp_image_2023-12-06_at_1006101.jpeg"
+      "imageURL":
+          "https://bpba.acehprov.go.id/thumbnail/700x0/media/2024.02/whatsapp_image_2023-12-06_at_1006101.jpeg"
     },
   ];
 
   var content = [
     Builder(
-    builder: (BuildContext context) =>     Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-          Padding(padding: EdgeInsets.fromLTRB(0,20,0,0), 
-          child:
-            Row(
+      builder: (BuildContext context) => Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
+            child: Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Column(
                   children: [
                     ElevatedButton(
-                      onPressed: () {}, 
-                      child: 
-                      Padding(
-                        padding: EdgeInsets.all(10),
-                        child: 
-                        Image(image: AssetImage("assets/images/clock_logo.png")),
-                      ),
-                      style: ElevatedButton.styleFrom(
-                      backgroundColor: Color(0xfff8ffd2),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                      fixedSize: Size(MediaQuery.of(context).size.width * 0.4, 100)
-                    )),
-                    SizedBox(height: 5,),
-                    Text("Laporan Terakhir",
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                    textAlign: TextAlign.start,),
+                        onPressed: () {},
+                        child: const Padding(
+                          padding: EdgeInsets.all(10),
+                          child: Image(
+                              image:
+                                  AssetImage("assets/images/clock_logo.png")),
+                        ),
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: const Color(0xfff8ffd2),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                            fixedSize: Size(
+                                MediaQuery.of(context).size.width * 0.4, 100))),
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    const Text(
+                      "Laporan Terakhir",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                      textAlign: TextAlign.start,
+                    ),
                   ],
                 ),
-
-                SizedBox(width: MediaQuery.of(context).size.width * 0.1,),
-
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.1,
+                ),
                 Column(
                   children: [
                     ElevatedButton(
-                      onPressed: () {}, 
-                      child: 
-                      Padding(
-                        padding: EdgeInsets.all(10),
-                        child: 
-                        Image(image: AssetImage("assets/images/report_logo.png")),
-                      ),
-                      style: ElevatedButton.styleFrom(
-                      backgroundColor: Color(0xfff8ffd2),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                      fixedSize: Size(MediaQuery.of(context).size.width * 0.4, 100)
-                    )),
-                    SizedBox(height: 5,),
-                    Text("Pelaporan",
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                    textAlign: TextAlign.start,),
+                        onPressed: () {},
+                        child: const Padding(
+                          padding: EdgeInsets.all(10),
+                          child: Image(
+                              image:
+                                  AssetImage("assets/images/report_logo.png")),
+                        ),
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: const Color(0xfff8ffd2),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                            fixedSize: Size(
+                                MediaQuery.of(context).size.width * 0.4, 100))),
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    const Text(
+                      "Pelaporan",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                      textAlign: TextAlign.start,
+                    ),
                   ],
                 ),
               ],
             ),
           ),
           Padding(
-            padding: EdgeInsets.fromLTRB(20,20,20,0),
+            padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
             child: Container(
               height: 50,
               decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: Color(0xffd0f288), 
+                borderRadius: BorderRadius.circular(10),
+                color: const Color(0xffd0f288),
               ),
             ),
           ),
-          Padding(
-            padding: EdgeInsets.fromLTRB(20,20,20,0),
+          const Padding(
+            padding: EdgeInsets.fromLTRB(20, 20, 20, 0),
             child: Column(children: [
-              Text("Latest news",
-              style: TextStyle(fontWeight: FontWeight.bold),
-              textAlign: TextAlign.start,),
+              Text(
+                "Latest news",
+                style: TextStyle(fontWeight: FontWeight.bold),
+                textAlign: TextAlign.start,
+              ),
             ]),
           ),
           Container(
             height: 200,
-            child: 
-            ListView(
+            child: ListView(
               scrollDirection: Axis.horizontal,
               shrinkWrap: true,
               children: news.map((data) {
-                return Padding(padding: EdgeInsets.fromLTRB(20,20,20,20),
-                  child: 
-                    Stack(
-                      children: [
-                      ClipRRect(borderRadius: BorderRadius.circular(20),
-                        child: Image.network(data['imageURL'], ),
+                return Padding(
+                  padding: const EdgeInsets.fromLTRB(20, 20, 20, 20),
+                  child: Stack(
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(20),
+                        child: Image.network(
+                          data['imageURL'],
+                        ),
                       ),
                       Positioned(
-                        left: 20,
-                        bottom: 20,
-                        child:                           
-                          Text(data['News'],
-                          style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),)
-                          ),
-                    ],)
-                  ,);
-                }).toList(),
-              ),
+                          left: 20,
+                          bottom: 20,
+                          child: Text(
+                            data['News'],
+                            style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white),
+                          )),
+                    ],
+                  ),
+                );
+              }).toList(),
+            ),
           ),
-        ],),
+        ],
+      ),
     ),
-    Center(child: Text("Maps"),),
-    Center(child: Text("Profil"),),
+    const Center(
+      child: Text("Maps"),
+    ),
+    Center(
+        child: Center(
+            child: Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Stack(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: const Color(0xffd0f288),
+                borderRadius: const BorderRadius.only(
+                  bottomLeft: Radius.circular(45.0),
+                  bottomRight: Radius.circular(45.0),
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.2),
+                    spreadRadius: 1,
+                    blurRadius: 2,
+                    offset: const Offset(0, 2),
+                  ),
+                ],
+              ),
+              child: const Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  SizedBox(height: 20.0),
+                  CircleAvatar(
+                    radius: 50.0,
+                    backgroundImage: AssetImage('assets/profile_picture.jpg'),
+                  ),
+                  SizedBox(height: 20.0),
+                  Text(
+                    'Nama Anda',
+                    style: TextStyle(
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(height: 10.0),
+                  Text(
+                    'Email Anda',
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      color: Colors.grey,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
+            ),
+            Positioned(
+              bottom: 5.0,
+              right: 5.0,
+              child: ElevatedButton(
+                onPressed: () {
+                  // Tambahkan fungsi yang akan dijalankan saat tombol ditekan
+                },
+                style: ElevatedButton.styleFrom(
+                  shape: const CircleBorder(),
+                  padding: const EdgeInsets.all(8.0),
+                  backgroundColor: Color(0xFF8ADAB2),
+                ),
+                child: const Icon(
+                  Icons.live_help,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ],
+        ),
+        Container(
+          width: double.infinity,
+          // Set lebar Container menjadi sepanjang layar
+          padding: const EdgeInsets.all(16.0),
+          // Atur padding sesuai kebutuhan
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+            ElevatedButton(
+              onPressed: () {
+                // Tambahkan fungsi yang akan dijalankan saat tombol ditekan
+              },
+              child: const Text('Account'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // Tambahkan fungsi yang akan dijalankan saat tombol ditekan
+              },
+              child: const Text('App Setting'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // Tambahkan fungsi yang akan dijalankan saat tombol ditekan
+              },
+              child: const Text('Add Device'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // Tambahkan fungsi yang akan dijalankan saat tombol ditekan
+              },
+              child: const Text('Feedback'),
+            ),
+          ]),
+        ),
+        const SizedBox(height: 20.0),
+        ElevatedButton(
+          onPressed: () {
+            // Tambahkan fungsi untuk meng-handle logout atau navigasi ke halaman pengaturan
+          },
+          child: const Text('Logout'),
+        ),
+      ],
+    ))),
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          leading: Padding(padding: EdgeInsets.fromLTRB(10,10,0,10),
-          child: Image(image: AssetImage("assets/images/logononcut.png"),)),
-          title: Text("SafeFlare",
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-          )),
-          backgroundColor: Color(0xffd0f288),
-        ),
-        body: content[_currentIndex],
-        backgroundColor: Colors.white,
-        bottomNavigationBar: BottomNavigationBar(currentIndex: _currentIndex, 
-        items: [
+      appBar: AppBar(
+        leading: const Padding(
+            padding: EdgeInsets.fromLTRB(10, 10, 0, 10),
+            child: Image(
+              image: AssetImage("assets/images/logononcut.png"),
+            )),
+        title: const Text("SafeFlare",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+            )),
+        backgroundColor: const Color(0xffd0f288),
+        actions: <Widget>[
+          Stack(
+            alignment: Alignment.center,
+            children: <Widget>[
+              IconButton(
+                icon: Icon(Icons.notifications), // Ikon notifikasi
+                onPressed: () {
+                  // Logika yang akan dijalankan saat tombol ditekan
+                },
+              ),
+              Positioned(
+                top: 8,
+                right: 8,
+                child: Container(
+                  padding: EdgeInsets.all(2),
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.circular(90),
+                  ),
+                  constraints: BoxConstraints(
+                    minWidth: 14,
+                    minHeight: 14,
+                  ),
+                  child: Center(
+                    child: Text(
+                      '5', // Angka notifikasi (ganti sesuai kebutuhan)
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 8,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+      body: content[_currentIndex],
+      backgroundColor: Colors.white,
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        items: const [
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
             label: "Home",
@@ -173,8 +347,8 @@ class _Homepage extends State<Homepage> {
             _currentIndex = index;
           });
         },
-        backgroundColor: Color(0xffd0f288),
-        ),
-      );
+        backgroundColor: const Color(0xffd0f288),
+      ),
+    );
   }
 }
